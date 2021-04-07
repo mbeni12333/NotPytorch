Fixed gradient flow

src/main.py
@@ -3,6 +3,7 @@ from src.nottorch.Datasets.loader import *
 from src import nottorch
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 # In[] model linear
 
@@ -128,11 +129,40 @@ alltrainx,alltrainy = load_usps("train")
 alltestx,alltesty = load_usps("test")
 neg, pos = 5, 6
 
-datax,datay = get_usps([neg,pos],alltrainx,alltrainy)
+trainx,trainy = get_usps([neg,pos],alltrainx,alltrainy)
 testx,testy = get_usps([neg,pos],alltestx,alltesty)
 
+mu = trainx.mean(axis=0)
+sig = trainx.std(axis=0)
 
-datay = np.where(datay == pos, 1, 0)
+trainx = (trainx - mu)/sig
+testx = (testx - mu)/sig
+
+trainy = np.where(trainy == pos, 1, 0)
 testy = np.where(testy == pos, 1, 0)
 
 show_usps(datax, datay, rows=16, cols=32)
+
+# In[]
+
+
+epochs = 1000
+
+model = nn.Sequential([nn.Linear(256, 128),
+                       nn.ReLU(),
+                       nn.Linear(128, 64),
+                       nn.ReLU(),
+                       nn.Linear(64, 1)])
+
+
+Criterion = nn.BCELoss()
+
+
+losses = utils.train(model, trainx, trainy, Criterion, epochs=epochs, print_every=10)
+
+
+utils.plot_losses((losses))
+# In[]
+
+yhat = np.where(nn.F.sigmoid(model(testx)) >= 0.5, 1, 0)
+utils.plot_report(testy, yhat, [neg, pos])

src/nottorch/nn.py
@@ -153,9 +153,8 @@ class Linear(Module):
         self._parameters = {}
         self._gradients = {}
 
-        self._parameters["W"] = np.random.randn(
-            in_dim, out_dim) * np.sqrt(2.0 / in_dim)
-        self._parameters["b"] = np.ones((1, out_dim))
+        self._parameters["W"] = np.random.randn(in_dim,out_dim) * np.sqrt(2.0 / in_dim)
+        self._parameters["b"] = np.zeros((1, out_dim))
 
         return
 
@@ -171,7 +170,6 @@ class Linear(Module):
     def zero_grad(self):
 
         if self._gradients is not None:
-
             self._gradients["W"] = np.zeros(self._parameters["W"].shape)
             self._gradients["b"] = np.zeros(self._parameters["b"].shape)
 
@@ -186,7 +184,7 @@ class Linear(Module):
         batch_size = dZ.shape[0]
 
         self._gradients["W"] += A.T @ dZ / batch_size
-        self._gradients["b"] += dZ.mean(0)
+        self._gradients["b"] += dZ.mean(0, keepdims=True)
 
         return dZ @ self._parameters["W"].T
 
@@ -232,7 +230,7 @@ class Tanh(Module):
         super().__init__()
 
     def forward(self, X):
-        return 2.0 / (1 + np.exp(-2*X)) - 1
+        return 2.0 / (1.0 + np.exp(-2.0*X)) - 1.0
             
 
     def backward_update_gradient(self, Z, dA):
@@ -243,7 +241,7 @@ class Tanh(Module):
 
     def backward_delta(self, Z, dA, lr=1e-3):
         A = self.forward(Z)
-        return 1 - np.square(A)
+        return (1.0 - np.square(A)) * dA
     
     def update_parameters(self, gradient_step=1e-3):
         return
@@ -267,7 +265,7 @@ class ReLU(Module):
         return dZ
 
     def backward_delta(self, Z, dA):
-        return dA * (Z <= 0.0)
+        return dA * (Z > 0.0)
     
     def update_parameters(self, gradient_step=1e-3):
         return

src/nottorch/utils.py
@@ -2,8 +2,24 @@ from . import nn
 import numpy as np
 from sklearn.datasets import make_regression, load_diabetes, make_classification
 import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, classification_report
+import seaborn as sns
+import pandas as pd
 
 
+def plot_report(y, y_hat, labels):
+    """
+    
+    """
+    
+    cm = confusion_matrix(y, y_hat)
+    cr = classification_report(y, y_hat, output_dict=True)
+    
+    plt.figure(figsize=(20,10))
+    plt.subplot(121)
+    sns.heatmap(cm, annot=True)
+    plt.subplot(122)
+    sns.heatmap(pd.DataFrame(cr).iloc[:-1, :].T, annot=True)
 
 
 def generateBatches(trainx, trainy, batch_size=32, shuffle=True):
@@ -114,7 +130,7 @@ def one_hot_encode(y):
     """
     pass
 
-def train(model:nn.Module, X, Y, Criterion, Optim=None, epochs=1000, verbose=False):
+def train(model:nn.Module, X, Y, Criterion, Optim=None, epochs=1000, verbose=False, print_every=100):
     """
     
 
@@ -137,7 +153,7 @@ def train(model:nn.Module, X, Y, Criterion, Optim=None, epochs=1000, verbose=Fal
     
     for epoch in range(epochs):
         
-        print(f"Epoch : {epoch}/{epochs})")
+        
 
         Yhat = model(X)
         
@@ -145,13 +161,14 @@ def train(model:nn.Module, X, Y, Criterion, Optim=None, epochs=1000, verbose=Fal
         
         losses.append(loss)
         
-        print(f"loss = {loss}")
+        if epoch % print_every == 0:
+            print(f"Epoch : {epoch}/{epochs}, loss = {loss}")
+            
         
-        dYhat = Criterion.backward(Y, Yhat)
         
         model.zero_grad()
         
-        print(dYhat.shape, X.shape)
+        dYhat = Criterion.backward(Y, Yhat)
         
         model.backward_update_gradient(X, dYhat)
         